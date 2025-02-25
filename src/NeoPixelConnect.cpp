29fdd27aa173337ca8dc480575abd924976a4ab8
MrYsLab <MisterYsLab@gmail.com>
2025-02-25 14:23:48 -0500

    Merge of Pull Request #11

--- src/NeoPixelConnect.cpp
+++ src/NeoPixelConnect.cpp
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2020-2022 Alan Yorinks All rights reserved.
+ Copyright (c) 2020-2024 Alan Yorinks All rights reserved.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
